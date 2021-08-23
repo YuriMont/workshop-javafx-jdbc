dc849d29442209cbbe7fa6d7199c7132b91deedc
YuriMont <81181134+YuriMont@users.noreply.github.com>
2021-08-23 08:27:09 -0300

    Atualizado

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -8,7 +8,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -73,17 +76,69 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
     @Override
     public void deleteById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+            st.setInt(1, id);
+            int linhas = st.executeUpdate();
+            if(linhas>0){
+                System.out.println("Excluido com sucesso!");
+            }else{
+                throw new DbException("Erro inesperado nenhuma linha foi alterada");
+            }
+        }catch(SQLException e){
+            throw new DbException("Erro: "+e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public Department findById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement("SELECT * FROM department WHERE Id = ? ORDER BY Nome");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if(rs.next()){
+                Department dep = instatieteDepartment(rs);
+                return dep;
+            }
+            return null;
+        }catch(SQLException e){
+            throw new DbException("Erro: "+e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
     public List<Department> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement("SELECT * FROM department");
+            rs = st.executeQuery();
+            
+            List<Department>list = new ArrayList<>();
+            Map<Integer, Department>map = new HashMap<>();
+            while(rs.next()){
+                Department dep = map.get(rs.getInt(1));
+                if(dep == null){
+                    dep = instatieteDepartment(rs);
+                    map.put(rs.getInt(1), dep);
+                }
+                list.add(dep);
+            }
+            return list;
+        }catch(SQLException e){
+            throw new DbException("Erro: "+e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
     
     private Department instatieteDepartment(ResultSet rs) throws SQLException{
